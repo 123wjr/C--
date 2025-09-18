bccd63bb913c8042bb5b11da1aad641fdc46e18e
John Jerry <jerry.only@qq.com>
2025-09-18 17:47:38 +0800

    5-3

---
+++ example_5-3.cpp
@@ -0,0 +1,45 @@
+/* #include <iostream>
+int main(){
+    int i=1,n,a[200];
+    std::cin >> n;
+    a[0]=n;
+    do {
+    if (n%2) {
+        n/=2;
+        a[i++]=n;
+    }else {
+        n=n*3+1;
+        a[i++]=n;
+    }
+    }while (n!=1);
+    i--;
+    do {
+    std::cout << a[i--];
+    }while (i--);
+    return 0;
+} */
+
+#include <cstdio>
+int main(){
+    int n,a[210],i=1;
+    scanf("%d",&n);
+    if (n==1) {
+        printf("%d",1);
+    }else {
+    a[0]=n;
+    while (n!=1) {
+        if (n%2) {
+            n/=2;
+            a[i++]=n;
+        }else {
+            n=3*n+1;
+            a[i++]=n;
+        }
+    }
+    i--;
+    while (i--) {
+    printf("%d ",a[i]);
+    }
+    }
+    return 0;
+}
