8978d3b2ff4469e68b03d2a98b686448f6742c15
John Jerry <jerry.only@qq.com>
2025-09-19 13:19:24 +0800

    5-8

--- example_5-8.cpp
+++ example_5-8.cpp
@@ -1,9 +1,9 @@
 #include <cstdio>
 int main(){
-    int n,p[7],a[7]={0,0,0,0,0,0,0},sum[7]={0,0,0,0,0,0,0},tem=0;
+    int n,p[7],a[7]={0,0,0,0,0,0,0},sum[8]={0,0,0,0,0,0,0,0},tem=0;
     scanf("%d",&n);
     scanf("%d %d %d %d %d %d %d",&p[0],&p[1],&p[2],&p[3],&p[4],&p[5],&p[6]);
-    for (int i=0; i<7; i++) {
+    for (int i=0; i<n; i++) {
         tem = 0;
         scanf("%d %d %d %d %d %d %d",&a[0],&a[1],&a[2],&a[3],&a[4],&a[5],&a[6]);
         for (int j=0; j<7; j++) {
@@ -13,7 +13,7 @@ int main(){
                 }
             }
         }
-        sum[7-tem]++;
+        sum[7-tem]++; // sum[7-tem]++ 在tem=0时越界，故扩大sum的范围
     }
     for (int i=0; i<7; i++) {
         printf("%d ",sum[i]);
