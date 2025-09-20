c8edba1262a87dd17d14e417e25a0a60f3bb9b49
John Jerry <jerry.only@qq.com>
2025-09-20 13:50:02 +0800

    exercise_5-2

---
+++ work_5-2.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstring>
+
+int a[101],b[101];
+
+int main(){
+	int n,ans=0;
+	scanf("%d",&n);
+	for (int i=0; i<n; i++) {
+		scanf("%d",&a[i]);
+	}
+	for (int i=1; i<n-1; i++) {
+		for (int j=0; j<i; j++) {
+			for (int k=i; k<n ; k++) {
+				if (a[i]+a[j]==a[k]) {
+					ans++;
+					b[k]++;
+				}
+			}
+		}
+	}
+	for (int i=0; i<n; i++) {
+		if (b[i]>=2) {
+			ans-=(b[i]-1);
+		}
+	}
+	printf("%d",ans);
+	return 0;
+}
