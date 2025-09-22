a4c30ae8d0661b3f75f437c6e6c50f47f25830a5
John Jerry <jerry.only@qq.com>
2025-09-22 21:25:36 +0800

    5-5完成

--- work_5-5.cpp
+++ work_5-5.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 
 int main(){
-    int n,tem=0;
+    int n,tem=0,target;
     scanf("%d",&n);
     double a[n+1];
     int t[n+1];
@@ -15,6 +15,20 @@ int main(){
     int state[tem+1];
     memset(state, 0, sizeof(state));
     for (int i=0; i<n; i++) {
-        
+        for (int j=1; j<=t[i]; j++) {
+			target=(j*a[i])/1;
+			if (state[target]) {
+				state[target]=0;
+			}else {
+				state[target]=1;
+			}
+		}
     }
+	for (int i=0; i<tem+1; i++) {
+		if (state[i]) {
+			printf("%d",i);
+			break;
+		}
+	}
+	return 0;
 }
