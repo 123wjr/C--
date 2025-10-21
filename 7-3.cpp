f293496089cb0f4c0fb8e4b839b981800c4feedf
John Jerry <jerry.only@qq.com>
2025-10-21 18:28:56 +0800

    优化代码格式，调整缩进和空格以提高可读性

--- 7-3.cpp
+++ 7-3.cpp
@@ -3,18 +3,24 @@ int main()
 {
 	int m, n, k, tem, wins = 0, acc = 0;
 	scanf("%d %d %d", &n, &m, &k);
-	for (int i=0; i<n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		scanf("%d", &tem);
 		wins += tem;
-		acc=(tem==1)?acc+1:0;
+		acc = (tem == 1) ? acc + 1 : 0;
 	}
-	if (acc>=k) {
+	if (acc >= k)
+	{
 		wins += ((m % (k + 1) == 0) ? 0 : m % (k + 1) - 1) + (m / (k + 1)) * k;
-	}else if (acc==wins && wins!=0) {
+	}
+	else if (acc == wins && wins != 0)
+	{
 		wins = ((n + m) % (k + 1) == 0)
 				   ? (n + m) / (k + 1) * k
 				   : (n + m) % (k + 1) + ((n + m) / (k + 1)) * k;
-	}else {
+	}
+	else
+	{
 		wins += (((m + acc) % (k + 1) == 0) ? 0 : (m + acc) % (k + 1)) +
 				((m + acc) / (k + 1)) * k - acc;
 	}
