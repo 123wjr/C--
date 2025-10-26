6526d4f521f32f237b6a7ee7211bbda48893fa4b
John Jerry <jerry.only@qq.com>
2025-10-26 21:20:41 +0800

    a not completed solution

--- P7910.cpp
+++ P7910.cpp
@@ -1,22 +1,91 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
+#include <utility>
 struct list
 {
 	int value;
 	int index;
-} a[8001], b[8001];
-
+} a[8001];
+int c[8001];
 int n, q;
 
-void change()
+void range_bubble_sort(bool from_start, int pos)
+{
+	if (from_start)
+	{
+		int start = 1;
+		for (int i = 1; i < c[pos]; i++)
+		{
+			if (a[i].value > a[c[pos]].value ||
+				(a[i].value == a[c[pos]].value && a[i].index > pos))
+			{
+				start = i;
+				break;
+			};
+		}
+		for (int i = c[pos]; i > start; i--)
+		{
+			std::swap(a[i], a[i - 1]);
+			std::swap(c[a[i].index], c[a[i - 1].index]);
+		}
+	}
+	else
+	{
+		int end = n;
+		for (int i = c[pos] + 1; i <= n; i++)
+		{
+			if (a[i].value > a[c[pos]].value ||
+				(a[i].value == a[c[pos]].value && a[i].index > pos))
+			{
+				end = i - 1;
+				break;
+			};
+		}
+		for (int i = c[pos]; i < end; i++)
+		{
+			std::swap(a[i], a[i + 1]);
+			std::swap(c[a[i].index], c[a[i + 1].index]);
+		}
+	}
+}
+
+void change_sort()
 {
 	int x, v;
 	scanf("%d %d", &x, &v);
-	a[x].value = v;
+	a[c[x]].value = v;
+	if (v < a[c[x] - 1].value)
+	{
+		range_bubble_sort(true, x);
+	}
+	else if (v == a[c[x] - 1].value)
+	{
+		if (a[c[x]].index < a[c[x] - 1].index)
+		{
+			std::swap(a[c[x]], a[c[x] - 1]);
+			std::swap(c[a[c[x]].index], c[a[c[x] - 1].index]);
+		}
+	}
+	else if (v < a[c[x] + 1].value)
+	{
+		return;
+	}
+	else if (v == a[c[x] + 1].value)
+	{
+		if (a[c[x]].index > a[c[x] + 1].index)
+		{
+			std::swap(a[c[x]], a[c[x] + 1]);
+			std::swap(c[a[c[x]].index], c[a[c[x] + 1].index]);
+		}
+	}
+	else
+	{
+		range_bubble_sort(false, x);
+	}
 }
 
-bool camp(list i, list j)
+bool comp(list i, list j)
 {
 	if (i.value != j.value)
 	{
@@ -25,16 +94,11 @@ bool camp(list i, list j)
 	return i.index < j.index;
 }
 
-void sort_find()
+void find_ori()
 {
 	int tem;
 	scanf("%d", &tem);
-	for (int i = 1; i <= n; i++)
-	{
-		b[i] = a[i];
-	}
-	std::sort(b + 1, b + n + 1, camp);
-	printf("%d\n", b[tem].index);
+	printf("%d\n", c[tem]);
 }
 
 int main()
@@ -45,14 +109,19 @@ int main()
 		scanf("%d", &a[i].value);
 		a[i].index = i;
 	}
+	std::sort(a + 1, a + n + 1, comp);
+	for (int i = 1; i <= n; i++)
+	{
+		c[a[i].index] = i;
+	}
 	for (int i = 1; i <= q; i++)
 	{
 		int tem;
 		scanf("%d", &tem);
 		switch (tem)
 		{
-		case 1: change(); break;
-		case 2: sort_find(); break;
+		case 1: change_sort(); break;
+		case 2: find_ori(); break;
 		}
 	}
 }
